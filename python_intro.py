c771dc730c425e9b96c10ab411fe73b537b08277
Helen C <helen.chapman@torchbox.com>
2022-07-05 15:32:09 +0100

    My Django Girls app, first commit

---
+++ python_intro.py
@@ -0,0 +1,11 @@
+def hi(name):
+    print('Hi there ' + name)
+    print('alright?')
+    print('not so bad nice day for it')
+
+girls = ['Rachel', 'Monica', 'Phoebe', 'Ola', 'You']
+for name in girls:
+    hi(name)
+    print('Next girl')
+
+participants = {'name': 'Helen', 'country': 'UK', 'favorite_numbers': [7, 42, 92]}
