5929ba8798c178244a9992afd5ee06eef114fc58
Helen C <helen.chapman@torchbox.com>
2022-10-09 17:42:49 +0100

    use view to query the model and get a queryset, and set it to the template

--- blog2/views.py
+++ blog2/views.py
@@ -1,6 +1,14 @@
 from django.shortcuts import render
+from django.utils import timezone
+from .models import Post
+
 
 # Create your views here.
 
+
 def post_list(request):
-    return render(request, 'blog2/post_list.html', {})
+    # get a QuerySet of posts that aren't in the future,
+    # ordered by when they were published
+    posts = Post.objects.filter(published_date__lte=timezone.now()).order_by('published_date')
+    # pass the posts to the template
+    return render(request, 'blog2/post_list.html', {'posts': posts})
